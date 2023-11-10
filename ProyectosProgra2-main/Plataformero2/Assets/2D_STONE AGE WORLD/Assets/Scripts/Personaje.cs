e781eaa28b6601df6506604444a6900117f11cea
Flawersaurio <142053653+Flawersaurio@users.noreply.github.com>
2023-11-10 10:48:53 -0600

    Plataformero final
    A

---
+++ ProyectosProgra2-main/Plataformero2/Assets/2D_STONE AGE WORLD/Assets/Scripts/Personaje.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Personaje : MonoBehaviour
+{
+    public int hp =60; 
+    public int hpMax = 100;
+    public int score = 0;
+    public int vidas = 3;
+
+    public GameObject efectoSangrePrefab;
+  
+    private Animator miAnimador;
+    private ReproductorSonidos misSonidos;
+
+    void Start()
+    {
+        miAnimador = GetComponent<Animator>();
+        misSonidos = GetComponent<ReproductorSonidos>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+       
+    }
+
+    public void hacerDanio(int puntos, GameObject atacante )
+    {
+        print(name + " recibe daño de " + puntos + " por " + atacante.name);
+
+        
+        //resto los puntos al HP actual
+        hp = hp - puntos;
+
+        if (miAnimador != null)
+        {
+            miAnimador.SetTrigger("DAÑAR");
+        }
+
+        //Creo una instancia de la particula de sangre
+        GameObject sangre = Instantiate(efectoSangrePrefab, transform);
+
+        if (misSonidos != null)
+        {
+            misSonidos.reproducir("DAÑAR");
+        }
+
+    }
+
+    public void matar(GameObject atacante)
+    {
+        print(name + " ha sido matado por " + atacante.name);
+        hp = 0;
+        vidas--;
+
+        misSonidos.reproducir("MORIR");
+    }
+
+}
---
+++ ProyectosProgra2-main/ProyectosProgra2-main/Plataformero2/Assets/2D_STONE AGE WORLD/Assets/Scripts/RabanoEnemigo.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RabanoEnemigo : MonoBehaviour
+{
+    public float velocidadCaminar = 1.5f;
+    public float distanciaAgro = 5;
+    private Rigidbody2D miCuerpo;
+    private Animator miAnimador;
+    private GameObject heroe;
+
+    public int puntosDanio = 10;
+
+    void Start()
+    {
+        miCuerpo = GetComponent<Rigidbody2D>();
+        miAnimador = GetComponent<Animator>();
+        heroe = GameObject.FindWithTag("Player");
+    }
+
+    void Update()
+    {
+        Vector3 posHeroe = heroe.transform.position;
+        Vector3 posYo = this.transform.position;
+
+        float distancia = (posYo - posHeroe).magnitude;
+        float velActualVert = miCuerpo.velocity.y;
+
+        if (distancia < distanciaAgro)
+        {
+            if (posHeroe.x > posYo.x)
+            {
+                transform.rotation = Quaternion.Euler(0, 180, 0);
+               
+            }
+
+            else
+            {
+                transform.rotation = Quaternion.Euler(0, 0, 0);
+              
+            }
+        }
+
+      
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        print(name + " hizo collisión con " + collision.gameObject.name);
+
+        GameObject otro = collision.gameObject;
+
+        if (otro.tag == "Player")
+        {
+            Personaje elPerso = otro.GetComponent<Personaje>();
+            elPerso.hacerDanio(puntosDanio, this.gameObject);
+        }
+    }
+}
+
